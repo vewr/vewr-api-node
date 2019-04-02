1cb456c391832ccb2070878122b783e99de97a54
dark-swordsman <kylebarr54321@gmail.com>
2019-04-01 22:17:48 -0400

    make alphaKey key field unique since there can be 3.358155651×10^64 combinations and it makes look ups faster

--- models/alphaKey.js
+++ models/alphaKey.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var alphaKeySchema = new Schema({
-  key: { type: String, required: true, trim: true }
+  key: { type: String, required: true, unique: true, trim: true }
 }, {
   timestamps: true
 });
