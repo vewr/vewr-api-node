615c4de02af38d36d7d352bb6675d4af0d531811
dark-swordsman <kylebarr54321@gmail.com>
2019-04-01 21:05:37 -0400

    update user model so usernames are unique

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  username: { type: String, required: true, trim: true },
+  username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true, select: false },
   _videos: [{ type: Schema.Types.ObjectId, ref: 'Video', trim: true }],
   _alphaKey: { type: Schema.Types.ObjectId, ref: 'AlphaKey', trim: true },
