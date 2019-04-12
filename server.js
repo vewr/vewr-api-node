9dddc2bb6773f3e9f8e3ac60910b12c6d2bf938c
dark-swordsman <kylebarr54321@gmail.com>
2019-04-12 13:26:44 -0600

    turn off cors repo?

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var mongoConfig = {
 server.use(bodyParser.json());
 // initalize our authentication strategy
 server.use(auth.init());
-server.use(cors());
+// server.use(cors());
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, FETCH, OPTIONS")
